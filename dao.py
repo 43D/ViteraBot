3f7d8c5bd1d92b7384a56127a2fd974899dff36f
Allan <allan.goioere@hotmail.com>
2023-08-31 09:01:07 -0300

    Uploader sample

--- dao.py
+++ dao.py
@@ -1,4 +1,6 @@
 import sqlite3
+import os
+import json
 
 class Dao:
     def __init__(self):
@@ -6,10 +8,24 @@ class Dao:
         sqlite3.connect(self.db)
         self._folder()
 
+    def _checkConfig(self):
+        if not os.path.exists('config.json'):
+            config_data = {
+                "app_id":"",
+                "app_secret":"",
+                "access_token":"",
+                "time_post": 180
+            }
+            with open('config.json', 'w') as config_file:
+                json.dump(config_data, config_file, indent=4)
+
     def _folder(self):
+        self._checkConfig()
         #check folder exits
         #check and save database folders local
         print("teste")
-        
+    
+    
+
     def getConn(self):
         return sqlite3.connect(self.db)
---
+++ uploader/controller/uploader.py
@@ -0,0 +1,19 @@
+from pyfacebook import GraphAPI
+
+class Uploader:
+    def __init__(self, config):
+        self.app_id = config["app_id"]
+        self.app_secret = config["app_secret"]
+        self.access_token = config["access_token"]
+        self.api = GraphAPI(
+            app_id=self.app_id,
+            app_secret=self.app_secret,
+            access_token=self.access_token
+        )
+
+    def uploadImage(self, message, filename):
+        self.api.post_object(object_id="me", connection="photos",
+                data={"message": message, "published": True},
+                files={"image": (filename, open(filename,'rb'), 'image/png')}
+        )
+        print(f"File ${filename} is uploaded")
