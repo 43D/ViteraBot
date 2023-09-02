51cd9b215802e6d47ca0c04c5daa965b9c5d1992
Allan <allan.goioere@hotmail.com>
2023-09-02 05:15:10 -0300

    delete bolso

---
+++ viterabot/controller/generator/generatorLero.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class generatorLero():
+    def __init__(self, filename) -> None:
+        self.filename = filename
+    
+    def _openFile(self):
+        bolsonaro = open(r'D:\\Allan\\Desktop\\bot\\viterabot\\bolsonaro.txt', encoding='utf8').read()
+        pass
+
+    
