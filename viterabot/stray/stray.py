2a0daa28b2e19d8f81da43539458090d7683adaf
Allan <allan.goioere@hotmail.com>
2023-09-17 21:01:21 -0300

    PT Up

--- viterabot/stray/stray.py
+++ viterabot/stray/stray.py
@@ -1,3 +1,5 @@
+import os
+import subprocess
 import pystray
 from PIL import Image
 from viterabot.observer.subject import Subject
@@ -18,6 +20,7 @@ class Stray:
 
     def _create_menu(self) -> None:
         self.menu = pystray.Menu(
+            pystray.MenuItem('Abrir explorador de arquivo', self._openFileExplorer, default=True),
             pystray.MenuItem('Gerar arquivo de config.', pystray.Menu(
                 pystray.MenuItem('GERAR __IMAGEM.json', self.subject.notify("")),
                 pystray.MenuItem('GERAR __TEMPLATE.json', self.subject.notify("")),
@@ -29,8 +32,14 @@ class Stray:
                 pystray.MenuItem('REGISTRAR MASCARA', self.subject.notify(""))
             )),
             pystray.MenuItem(lambda text: f'Logado: {self.logado}', self._update),
-            pystray.MenuItem('Close', self._close)
+            pystray.MenuItem('Fechar ViteraBot', self._close)
         )
+    def _openFileExplorer(self) -> None:
+        projeto_principal = os.getcwd()
+        if os.name == 'nt':
+            subprocess.Popen(['explorer', projeto_principal])
+        elif os.name == 'posix':
+            subprocess.Popen(['xdg-open', projeto_principal])
 
     def _update(self) -> None:
         self.logado = True
